f88867e89d24e6a42c4d1cafd5e4396c46e54096
TomayevTurxan <tu7hfn0xh@code.edu.az>
2023-11-20 12:35:39 -0800

    React material UI and comments completed by TT

--- React style, Material UI/src/components/Comment.jsx
+++ React style, Material UI/src/components/Comment.jsx
@@ -6,12 +6,42 @@ import Rating from "@mui/material/Rating";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { putAlbomsByID } from "../api/albums";
+import Snackbar from "@mui/material/Snackbar";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
 
 const Comment = ({ albom, handleClose3 }) => {
   const [comment, setComment] = useState("");
   const [rate, setRating] = useState("");
   const [value, setValue] = React.useState(2);
+  const [open, setOpen] = React.useState(false);
 
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+  const action = (
+    <React.Fragment>
+      <Button color="secondary" size="small" onClick={handleClose}>
+        UNDO
+      </Button>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
   return (
     <div>
       <InputLabel htmlFor="comment-input">Yorumunu yaz:</InputLabel>
@@ -40,6 +70,7 @@ const Comment = ({ albom, handleClose3 }) => {
       </Box>
       <Button
         onClick={() => {
+          handleClick();
           putAlbomsByID(albom.id, { comment, rate });
         }}
         variant="contained"
@@ -48,6 +79,7 @@ const Comment = ({ albom, handleClose3 }) => {
       >
         Created
       </Button>
+      {/* <Button onClick={handleClick}>Open simple snackbar</Button> */}
       <Button
         onClick={() => {
           handleClose3();
@@ -58,8 +90,15 @@ const Comment = ({ albom, handleClose3 }) => {
       >
         Close
       </Button>
+      <Snackbar
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        message="Comment elave olundu"
+        action={action}
+      />
+      ;
     </div>
   );
 };
-
 export default Comment;
